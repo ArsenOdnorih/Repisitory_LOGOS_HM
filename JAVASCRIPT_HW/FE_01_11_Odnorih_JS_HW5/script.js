fad961c23fbf335ee0c89a7eb90fc018c5d06538
ArsenOdnorih <arsenodnorih@gmail.com>
2023-02-21 08:38:38 +0200

    розкомітив таск 1 таск 2

--- JAVASCRIPT_HW/FE_01_11_Odnorih_JS_HW5/script.js
+++ JAVASCRIPT_HW/FE_01_11_Odnorih_JS_HW5/script.js
@@ -1,71 +1,71 @@
      //  TASK 1
 
-// let num = +prompt("Write the number of the month to find out its name.", "Number of the month");
+let num = +prompt("Write the number of the month to find out its name.", "Number of the month");
 
-// switch(num) {
-//     case 1:
-//         alert("January");
-//         break
-//     case 2:
-//         alert("February");
-//         break
-//     case 3:
-//         alert("March");
-//         break
-//     case 4:
-//         alert("April");
-//         break
-//     case 5:
-//         alert("May");
-//         break
-//     case 6:
-//         alert("June");
-//         break
-//     case 7:
-//         alert("July");
-//         break
-//     case 8:
-//         alert("August");
-//         break
-//     case 9:
-//         alert("September");
-//         break
-//     case 10:
-//         alert("October");
-//         break
-//     case 11:
-//         alert("November");
-//         break
-//     case 12:
-//         alert("December");
-//         break
-//     default:
-//         alert("Error");
-//         break
-// }
+switch(num) {
+    case 1:
+        alert("January");
+        break
+    case 2:
+        alert("February");
+        break
+    case 3:
+        alert("March");
+        break
+    case 4:
+        alert("April");
+        break
+    case 5:
+        alert("May");
+        break
+    case 6:
+        alert("June");
+        break
+    case 7:
+        alert("July");
+        break
+    case 8:
+        alert("August");
+        break
+    case 9:
+        alert("September");
+        break
+    case 10:
+        alert("October");
+        break
+    case 11:
+        alert("November");
+        break
+    case 12:
+        alert("December");
+        break
+    default:
+        alert("Error");
+        break
+}
 
 
     //  TASK 2
 
-// let writeNum = +prompt();
-// let counter = 0;
-// let res;
+let writeNum = +prompt();
+let counter = 0;
+let res;
 
-// function numberCheck() {
-//     for (let a = 1; a <= writeNum; a++ ) {
-//         if (writeNum % a === 0)
-//         counter++
-//     }
-//     if( counter<=2){
-//         res = "Просте";
-//     }else{
-//         res = "Склдне";
-//     }
-//     console.log(counter);
-//     alert(res)
-// }
+function numberCheck() {
+    for (let a = 1; a <= writeNum; a++ ) {
+        if (writeNum % a === 0)
+        counter++
+    }
+    if( counter<=2){
+        res = "Просте";
+    }else{
+        res = "Склдне";
+    }
+    console.log(counter);
+    alert(res)
+}
 
-// numberCheck();
+numberCheck();
 
 //  TASK 3 
 
