bbf3988d7443045aa631d28ae507e5ee7191c242
Arsen Odnorih <arsenodnorih@gmail.com>
2023-03-30 21:03:42 +0300

    coment

--- JS_Core_HW/FE_01_11_Odnorih_JS_Core_HW3/script.js
+++ JS_Core_HW/FE_01_11_Odnorih_JS_Core_HW3/script.js
@@ -1,8 +1,10 @@
-const person = {
-    userName: "Ivan",
-    usenAge: 25,
-    isDevloper: true,
-    'Email': '1@gmail.com',
-    null: function greeting() {console.log("");}
+// const person = {
+//     userName: "Ivan",
+//     usenAge: 25,
+//     isDevloper: true,
+//     'Email': '1@gmail.com',
+//     null: function greeting() {console.log("");}
 
-}
+// }
+
+const b = 12; 
